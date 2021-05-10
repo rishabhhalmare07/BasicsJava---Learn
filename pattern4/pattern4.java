56794e4a02bf4ea48fc63215ba7236ababf8b9e4
Rushabh Halmare <42460595+rishabhhalmare07@users.noreply.github.com>
2021-05-10 21:45:28 +0530

    Add files via upload
    Fifth Day

---
+++ pattern4/pattern4.java
@@ -0,0 +1,24 @@
+/**
+ * pattern4
+ */
+import java.util.Scanner;
+public class pattern4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int j = 1; j <= n; j++) {
+            
+            for (int i = 1; i <= j - 1; i++) {
+            System.out.print(" ");
+            }
+            for (int i = 1; i < n-j + 1; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    
+    
+    }
+}

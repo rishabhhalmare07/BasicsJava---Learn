172d52fd2b69eaff4972edb2ef488612980d0056
Rushabh Halmare <42460595+rishabhhalmare07@users.noreply.github.com>
2021-05-14 06:36:26 +0530

    Seventh Day

---
+++ array1/array1.java
@@ -0,0 +1,11 @@
+/**
+ * array1
+ */
+public class array1 {
+
+    public static void main(String[] args) {
+        int age[] = {10,14,22,33,47};
+        age[0] = 88;
+        System.out.println(age[0]);
+    }
+}
---
+++ array1/array2.java
@@ -0,0 +1,8 @@
+public class array2 {
+    public static void main(String[] args) {
+        int age [] = {2,7,22,75,19,27,67,27};
+           for (int i = 0; i < age.length; i++) {
+               System.out.println(age[i]);
+           }
+    }
+}
